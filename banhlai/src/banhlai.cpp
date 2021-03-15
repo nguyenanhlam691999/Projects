18ee57a273892586fbd417fe2d537990566e6d75
nalam <thecrystal2014@gmail.com>
2021-03-15 14:33:51 +0700

    2h33 file code 2 bánh trước

--- banhlai/src/banhlai.cpp
+++ banhlai/src/banhlai.cpp
@@ -1,77 +1,36 @@
 #include <Arduino.h>
+#include<my_lib.h>
+#include<SPI.h>
+#include<SoftwareSerial.h>
+#include"mcp2515.h"
+SoftwareSerial blue(2, 3);
 const int stepPin = 3; //PUL
 const int dirPin = 2;  // DIR
 const int enPin = 8;   //ena
-void queo_phai(const int dirr, const int stepPinn);
-void queo_trai(const int dirr, const int stepPinn);
+
+car nalam;
 void setup()
 {
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
   pinMode(enPin, OUTPUT);
   digitalWrite(enPin, LOW);
+
+  Serial.begin(9600);
+  blue.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
+  
 }
 void loop()
 {
+  nalam.queo_phai(dirPin, stepPin);
+  nalam.queo_trai(dirPin, stepPin);
 
-  queo_phai(dirPin, stepPin);
-  queo_trai(dirPin, stepPin);
-}
-
-void queo_phai(const int dirPinn, const int stepPinn)
-{
-  digitalWrite(dirPinn, HIGH);
-  for (char i = 0; i < 8; i++)
+  int tinhieudieukhien;
+  if (blue.available())
   {
-    for (int x = 0; x < 200; x++)
-    {
-      digitalWrite(stepPinn, HIGH);
-      delayMicroseconds(1000);
-      digitalWrite(stepPinn, LOW);
-      delayMicroseconds(1000);
-    }
-    delay(10);
-  }
-
-  digitalWrite(dirPinn, LOW);
-  for (char i = 0; i < 8; i++)
-  {
-    for (int x = 0; x < 200; x++)
-    {
-      digitalWrite(stepPinn, HIGH);
-      delayMicroseconds(1000);
-      digitalWrite(stepPinn, LOW);
-      delayMicroseconds(1000);
-    }
-    delay(10);
+    tinhieudieukhien=blue.read();
+    
   }
 }
 
-void queo_trai(const int dirPinn, const int stepPinn)
-{
-  digitalWrite(dirPinn, LOW);
-  for (char i = 0; i < 8; i++)
-  {
-    for (int x = 0; x < 200; x++)
-    {
-      digitalWrite(stepPinn, HIGH);
-      delayMicroseconds(1000);
-      digitalWrite(stepPinn, LOW);
-      delayMicroseconds(1000);
-    }
-    delay(10);
-  }
-  digitalWrite(dirPinn, HIGH);
-  for (char i = 0; i < 8; i++)
-  {
-    for (int x = 0; x < 200; x++)
-    {
-      digitalWrite(stepPinn, HIGH);
-      delayMicroseconds(1000);
-      digitalWrite(stepPinn, LOW);
-      delayMicroseconds(1000);
-    }
-    delay(10);
-  }
-}
