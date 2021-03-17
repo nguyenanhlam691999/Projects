ffafea4622901b45ee8077e22f553bd580400722
nalam <thecrystal2014@gmail.com>
2021-03-17 09:14:46 +0700

    quẹo

--- banhlai/src/banhlai.cpp
+++ banhlai/src/banhlai.cpp
@@ -1,36 +1,42 @@
 #include <Arduino.h>
-#include<my_lib.h>
-#include<SPI.h>
-#include<SoftwareSerial.h>
-#include"mcp2515.h"
-SoftwareSerial blue(2, 3);
+#include <my_lib.h>
+#include <SPI.h>
+#include "mcp2515.h"
+// can bus
+struct can_frame canMsg;
+MCP2515 mcp2515(10);
+// step motor
 const int stepPin = 3; //PUL
 const int dirPin = 2;  // DIR
 const int enPin = 8;   //ena
-
+// my_lib
 car nalam;
 void setup()
 {
+  Serial.begin(9600);
+  SPI.begin();
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_125KBPS);
+  mcp2515.setNormalMode();
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
   pinMode(enPin, OUTPUT);
   digitalWrite(enPin, LOW);
 
-  Serial.begin(9600);
-  blue.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  
+  pinMode(9, OUTPUT);
 }
 void loop()
 {
-  nalam.queo_phai(dirPin, stepPin);
-  nalam.queo_trai(dirPin, stepPin);
-
-  int tinhieudieukhien;
-  if (blue.available())
+  // nalam.queo_phai(dirPin, stepPin);
+  // nalam.queo_trai(dirPin, stepPin);
+  mcp2515.readMessage(&canMsg);
+  Serial.println(canMsg.can_id);
+  // neu id nhan = id gui mong muon
+  if ((canMsg.can_id == 0x0F6) && canMsg.data[0] == 1)
   {
-    tinhieudieukhien=blue.read();
-    
+    Serial.println(canMsg.can_id);
+    Serial.println(canMsg.data[0], HEX);
+    nalam.queo_phai(dirPin,stepPin,canMsg.data[0]);
   }
+  delay(1000);
 }
-
--- hc05/src/hc05.cpp
+++ hc05/src/hc05.cpp
@@ -1,22 +1,64 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
-#include"my_lib.h"
-SoftwareSerial blue(2, 3);
-custom_led ledblink;
+#include "my_lib.h"
+#include <SPI.h>
+#include "mcp2515.h"
+struct can_frame canMsg1;
+MCP2515 mcp2515(10);
+
+ SoftwareSerial blue(2, 3);
 void setup()
 {
+  // can bus
+  while (!Serial)
+    ;
   Serial.begin(9600);
+  SPI.begin();
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_125KBPS);
+  mcp2515.setNormalMode();
+  canMsg1.can_id = 0x0F6;
+  canMsg1.can_dlc = 8;
+  canMsg1.data[0] = 0;
+  canMsg1.data[1] = 0;
+  canMsg1.data[2] = 0;
+  canMsg1.data[3] = 0;
+  canMsg1.data[4] = 0;
+  canMsg1.data[5] = 0;
+  canMsg1.data[6] = 0;
+  canMsg1.data[7] = 0;
+  // bluetooth
   blue.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(9, OUTPUT);
 }
-
 void loop()
 {
-  int tinhieudieukhien;
+  int tinhieudieukhien=0;
   if (blue.available())
   {
-    tinhieudieukhien=blue.read();
-    ledblink.custom_led_run_condition(tinhieudieukhien,LED_BUILTIN);
+    tinhieudieukhien = blue.read();
+    
+    if (tinhieudieukhien == 100)
+    {
+      digitalWrite(9, HIGH);
+      delay(100);
+      // can bus
+      canMsg1.data[0] = 1;
+      mcp2515.sendMessage(&canMsg1);
+      delay(500);
+    
+    }
+    if (tinhieudieukhien == 200)
+    {
+      digitalWrite(9, LOW);
+      // can bus
+      canMsg1.data[0] = 0;
+      mcp2515.sendMessage(&canMsg1);
+      delay(500);
+      
+      
+    }
   }
-  
+Serial.println(tinhieudieukhien);
+  delay(100);
 }
