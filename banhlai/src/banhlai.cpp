a1e341e52f0c6f7f2204e192e0d324e70bb7d6f1
nalam <thecrystal2014@gmail.com>
2021-03-18 11:25:22 +0700

    queo tra

--- banhlai/src/banhlai.cpp
+++ banhlai/src/banhlai.cpp
@@ -6,14 +6,14 @@
 struct can_frame canMsg;
 MCP2515 mcp2515(10);
 // step motor
-const char stepPin = 3; //PUL
-const char dirPin = 2;  // DIR
-const char enPin = 8;   //ena
+const int stepPin = 3; //PUL
+const int dirPin = 2;  // DIR
+const int enPin = 8;   //ena
 // my_lib
 car nalam;
 // dem vong queo
-char demvongqueo = 0;
-char chieuvongqueo = 0;
+int demvongqueo = 0;
+int chieuvongqueo = 0;
 void setup()
 {
   // can bus
@@ -35,12 +35,12 @@ void loop()
 // queo phai
   while ((canMsg.can_id == 0x0F6) && canMsg.data[0] == 1)
   {
-    mcp2515.readMessage(&canMsg);
-    Serial.println(canMsg.data[0]);
+    
+    Serial.println(demvongqueo);
     chieuvongqueo = 1;
     nalam.queo_phai(dirPin, stepPin);
     Serial.println(" queo phai");
-    if (demvongqueo == 8 || canMsg.data[0] == 0)
+    if (demvongqueo==5)
     {
       Serial.println("ngung tang toc queo phai");
       break;
@@ -51,12 +51,12 @@ void loop()
   // queo trai
   while ((canMsg.can_id == 0x0F6) && canMsg.data[0] == 2)
   {
-    mcp2515.readMessage(&canMsg);
-    Serial.println(canMsg.data[0]);
+   
+    Serial.println(demvongqueo);
     chieuvongqueo = 2;
     nalam.queo_trai(dirPin, stepPin);
     Serial.println("queo trai");
-    if (demvongqueo == 8 || canMsg.data[0] == 0)
+    if (demvongqueo == 5)
     {
       Serial.println("ngung tang goc queo trai");
       break;
@@ -67,8 +67,8 @@ void loop()
   // tra lai
   while ((canMsg.can_id == 0x0F6) && canMsg.data[0] == 0)
   {
-    mcp2515.readMessage(&canMsg);
-    Serial.println(canMsg.data[0]);
+    
+    
     if (chieuvongqueo == 1)
     {
       nalam.tralai_phai(dirPin, stepPin);
@@ -79,7 +79,7 @@ void loop()
       nalam.tralai_trai(dirPin, stepPin);
        Serial.println("tra lai trai");
     }
-    if (demvongqueo == 0 || canMsg.data[0] != 0)
+    if (demvongqueo == 0 )
     {
       chieuvongqueo = 0;
       break;
